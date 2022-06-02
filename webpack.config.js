337155cdac7d792c0de4b1b15abbcf436d04d136
HomerMctavish <worffry@gmail.com>
2022-06-02 17:25:36 -0400

    added style and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,16 @@ module.exports ={
     entry: "./src/index.js",
     output:{
         filename: "main.js",
-        path: path.resolve(__dirname, "Dist")
+        path: path.resolve(__dirname, "dist")
     },
+    module:{
+        rules: [
+            {
+                test: /\.css$/,
+                //member css-loader loads first, and must be loaded first
+                use: ["style-loader", "css-loader"]
 
+            }
+        ]
+    }
 };
